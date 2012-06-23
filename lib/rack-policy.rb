75a3293b412ae1e2d85fc75b53b0cb001ee0c802
Piotr Murach <pmurach@gmail.com>
2012-06-23 12:06:50 +0100

    Initial project layout.

---
+++ lib/rack-policy.rb
@@ -0,0 +1,7 @@
+require "rack-policy/version"
+
+module Rack
+  module Policy
+    # Your code goes here...
+  end
+end
---
+++ lib/rack-policy/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Policy
+    VERSION = "0.0.1"
+  end
+end
