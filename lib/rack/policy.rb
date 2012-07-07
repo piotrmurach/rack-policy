217bf4aa4b7613903ecf69894548c243cc6f1902
Piotr Murach <pmurach@gmail.com>
2012-07-07 19:23:50 +0100

    Detect and load specific rack up extensions.

--- lib/rack/policy.rb
+++ lib/rack/policy.rb
@@ -7,6 +7,10 @@ module Rack
 
     autoload :CookieLimiter,  'rack/policy/cookie_limiter'
     autoload :Version,        'rack/policy/version'
+    autoload :Helpers,        'rack/policy/helpers'
+
+    # Initialize Rack::Policy extensions within an application
+    require 'rack/policy/extensions'
 
   end # Policy
 end # Rack
---
+++ lib/rack/policy/extensions.rb
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+
+# Autoload Rails extensions
+if defined?(Rails) && Rails.respond_to?(:application)
+  # Rails 3
+  require 'rack/policy/railtie'
+
+elsif defined?(Rails::Initializer)
+  # Rails 2.3
+  require 'action/view/base'
+
+  ActionView::Base.send :include, Rack::Policy::Helpers
+elsif defined?(Sinatra)
+  require 'rack/policy/sinatra'
+
+elsif defined?(Padrino)
+  require 'padrino-core'
+end
